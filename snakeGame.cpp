b01b9bc030e2163bd430bcf4b8b7ff26a7432c05
ActuallyMadhav <madhavra.2004@gmail.com>
2025-06-30 00:15:49 +0800

    0015 300625

---
+++ snakeGame.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+bool gameOver;
+
+void Setup(){
+    gameOver = false;
+
+}
+
+void Draw(){
+
+}
+
+void Input(){
+
+}
+
+void Logic(){
+
+}
+
+int main(){
+
+    while(!gameOver){
+        Draw();
+        Input();
+        Logic();
+        //Sleep(10);
+    }
+
+    return 0;
+}
